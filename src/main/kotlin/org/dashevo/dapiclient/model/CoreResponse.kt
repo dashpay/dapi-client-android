2327d51d5a8f00bbb8a27cccfc6ebffcc3312f8e
HashEngineering <hashengineeringsolutions@gmail.com>
2020-12-07 13:31:01 -0800

    CoreResponse: Add more information to GetStatusResponse

--- src/main/kotlin/org/dashevo/dapiclient/model/CoreResponse.kt
+++ src/main/kotlin/org/dashevo/dapiclient/model/CoreResponse.kt
@@ -7,7 +7,13 @@
 package org.dashevo.dapiclient.model
 
 import org.dashevo.dapiclient.provider.DAPIAddress
+import org.dashevo.dapiclient.DapiClient
 
+/**
+ * GetStatusResponse contains the response of the [DapiClient.getStatus] DAPI call with some additional
+ * information that includes the timeStamp when the call was made, the address of the masternode and the
+ * duration of the response time.
+ */
 data class GetStatusResponse(val coreVersion: Int,
                              val protocolVersion: Int,
                              val blocks: Int,
@@ -19,5 +25,6 @@ data class GetStatusResponse(val coreVersion: Int,
                              val relayFee: Double,
                              val errors: String,
                              val network: String,
+                             val timeStamp: Long,
                              val address: DAPIAddress? = null,
                              val duration: Long)
