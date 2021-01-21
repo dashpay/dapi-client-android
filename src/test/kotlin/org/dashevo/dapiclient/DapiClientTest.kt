a2524675ffa73dce63ab91e68a8f176483cb091e
HashEngineering <hashengineeringsolutions@gmail.com>
2021-01-21 10:06:58 -0800

    Add test for getMnListDiff

--- src/test/kotlin/org/dashevo/dapiclient/DapiClientTest.kt
+++ src/test/kotlin/org/dashevo/dapiclient/DapiClientTest.kt
@@ -6,15 +6,37 @@
  */
 package org.dashevo.dapiclient
 
-import org.bitcoinj.params.EvoNetParams
+import org.bitcoinj.params.TestNet3Params
 import org.junit.jupiter.api.Test
-import java.lang.Thread.sleep
 
 class DapiClientTest {
 
     @Test
     fun jRPCTests() {
-        val client = DapiClient(EvoNetParams.MASTERNODES[0])
+        val client = DapiClient(TestNet3Params.MASTERNODES.toList())
         println("blocking=> hash: " + client.getBestBlockHash())
+    }
+
+    fun getMnListDiff() : Map<String, Any> {
+        val client = DapiClient(TestNet3Params.MASTERNODES.toList())
+        val success = 0
+        do {
+            try {
+                val baseBlockHash = client.getBlockHash(0)
+                val blockHash = client.getBestBlockHash()
+                val mnListDiff = client.getMnListDiff(baseBlockHash!!, blockHash!!) as Map<String, Any>
+                return mnListDiff as Map<String, Any>
+            } catch (e: Exception) {
+                println("Error: $e")
+            }
+        } while (success == 0)
+        return mapOf()
+    }
+
+    @Test
+    fun getMnListDiffTest() {
+        val client = DapiClient(TestNet3Params.MASTERNODES.toList())
+        println("blocking=> hash: " + client.getBestBlockHash())
+        println(getMnListDiff()["mnList"])
     }
 }
