21a9a470acaf2458dd608550ae28106f29b1ed18
HashEngineering <hashengineeringsolutions@gmail.com>
2021-01-29 10:42:52 -0800

    Use moshi instead of gson for parsing Platform errors

--- src/main/kotlin/org/dashevo/dapiclient/model/GrpcExceptionInfo.kt
+++ src/main/kotlin/org/dashevo/dapiclient/model/GrpcExceptionInfo.kt
@@ -1,7 +1,10 @@
 package org.dashevo.dapiclient.model
 
 import io.grpc.StatusRuntimeException
-import org.json.JSONObject
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+
 
 class GrpcExceptionInfo (trailers: String) {
     val errors = arrayListOf<Map<String, Any>>()
@@ -14,9 +17,14 @@ class GrpcExceptionInfo (trailers: String) {
         val endpos = end?.first ?: trailers.length
         if (cursor != null) {
             val errorString = trailers.substring(cursor.first+ 7, endpos)
-
-            val errorMap = JSONObject("{errors: $errorString}").toMap()
-            errors.addAll(errorMap["errors"] as List<Map<String, Any>>)
+            val json = "{errors: $errorString }"
+            val moshi = Moshi.Builder().build()
+            val map = Types.newParameterizedType(MutableMap::class.java, String::class.java, Any::class.java)
+            val adapter: JsonAdapter<Map<String, Any>> = moshi.adapter<Map<String, Any>>(map).lenient()
+            val errorMap = adapter.fromJson(json)
+            if (errorMap != null && errorMap.containsKey("errors")) {
+                errors.addAll(errorMap["errors"] as List<Map<String, Any>>)
+            }
         }
     }
 
