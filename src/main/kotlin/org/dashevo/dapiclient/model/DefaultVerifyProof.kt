891e18e6cfba0035fd5a40b515f3b9a0c99f9d97
HashEngineering <hashengineeringsolutions@gmail.com>
2021-06-02 15:38:24 -0700

    refactor DefaultVerifyProof and add MerkLibVerifyProof.kt

--- src/main/kotlin/org/dashevo/dapiclient/model/DefaultVerifyProof.kt
+++ src/main/kotlin/org/dashevo/dapiclient/model/DefaultVerifyProof.kt
@@ -13,93 +13,115 @@ import org.dashevo.dpp.util.Cbor
 import org.dashj.merk.ByteArrayKey
 import org.dashj.merk.MerkVerifyProof
 
-class DefaultVerifyProof(private val stateTransition: StateTransitionIdentitySigned) : VerifyProof() {
+open class DefaultVerifyProof(protected val stateTransition: StateTransitionIdentitySigned) : VerifyProof() {
     override fun verify(proof: Proof): Boolean {
         val values = MerkVerifyProof.decode(proof.storeTreeProof)
         val hash = MerkVerifyProof.decodeRootHash(proof.storeTreeProof)
         if (values.isNotEmpty()) {
             when (stateTransition) {
                 is DocumentsBatchTransition -> {
-                    for (transition in stateTransition.transitions) {
-                        val rawDocument = HashMap(transition.toObject())
-                        rawDocument.remove("\$action")
-                        rawDocument["\$protocolVersion"] = stateTransition.protocolVersion
-                        rawDocument["\$ownerId"] = stateTransition.ownerId
-                        val rawDocumentFromProofBytes = values[ByteArrayKey(transition.id.toBuffer())]
-                        if (rawDocumentFromProofBytes != null) {
-                            val rawDocumentFromProof = Cbor.decode(rawDocumentFromProofBytes)
-                            when (transition) {
-                                is DocumentCreateTransition -> {
-                                    rawDocument.remove("\$entropy")
-                                    rawDocument["\$revision"] = 1
-                                }
-                                is DocumentReplaceTransition -> {
-                                    rawDocumentFromProof.remove("\$createdAt")
-                                }
-                                is DocumentDeleteTransition -> {
-
-                                }
-                            }
-                            val match = rawDocumentFromProof.keys == rawDocument.keys
-                            if (!match) {
-                                return false
-                            } else {
-                                for (key in rawDocumentFromProof.keys) {
-                                    val equal = when (rawDocumentFromProof[key]) {
-                                        is ByteArray -> {
-                                            when (rawDocument[key]) {
-                                                is ByteArray -> (rawDocumentFromProof[key] as ByteArray).contentEquals(
-                                                    rawDocument[key] as ByteArray
-                                                )
-                                                is Identifier -> (rawDocumentFromProof[key] as ByteArray).contentEquals(
-                                                    (rawDocument[key] as Identifier).toBuffer()
-                                                )
-                                                else -> {
-                                                    false
-                                                }
-                                            }
-                                        }
-                                        else -> {
-                                            rawDocumentFromProof[key] == rawDocument[key]
-                                        }
-                                    }
-                                    if (!equal) {
-                                        println("$key doesn't match in $rawDocumentFromProof and $rawDocument")
-                                        return false
-                                    }
-                                }
-                            }
-                        } else {
-                            return false
-                        }
-                    }
-                    return true
+                    return verifyDocumentsBatchTransition(stateTransition, values)
                 }
                 is DataContractCreateTransition -> {
-                    val match = Cbor.decode(values[values.keys.first()]!!) == stateTransition.dataContract.toObject()
-                    if (!match) {
-                        return false
-                    }
-                    return true
+                    return verifyDataContactCreateTransition(values[values.keys.first()]!!, stateTransition)
                 }
                 is IdentityCreateTransition -> {
-                    val rawIdentity = stateTransition.toObject()
-                    rawIdentity["id"] = stateTransition.identityId.toBuffer()
-                    rawIdentity["revision"] = 0
-                    rawIdentity.remove("signature")
-                    rawIdentity.remove("assetLockProof")
-                    rawIdentity.remove("type")
-
-                    val rawIdentityFromProof = Cbor.decode(values[values.keys.first()]!!)
-                    rawIdentityFromProof.remove("balance")
-                    val match = rawIdentityFromProof == rawIdentity
-                    if (!match) {
-                        return false
-                    }
-                    return true
+                    return verifyIdentityCreateTransition(stateTransition, values[values.keys.first()]!!)
                 }
             }
         }
         return false
+    }
+
+    private fun compareMaps(a: Map<String, Any?>, b: Map<String, Any?>): Boolean {
+        val match = a.keys == b.keys
+        if (!match) {
+            return false
+        } else {
+            for (key in a.keys) {
+                val equal = compareElements(a[key]!!, b[key]!!)
+                if (!equal) {
+                    println("$key doesn't match in $a and $b")
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun compareElements(a: Any, b: Any): Boolean {
+        return when (a) {
+            is ByteArray -> {
+                when (b) {
+                    is ByteArray -> a.contentEquals(b)
+                    is Identifier -> a.contentEquals(b.toBuffer())
+                    else -> {
+                        false
+                    }
+                }
+            }
+            else -> {
+                a == b
+            }
+        }
+    }
+
+    private fun verifyDocumentsBatchTransition(
+        stateTransition: DocumentsBatchTransition,
+        values: Map<ByteArrayKey, ByteArray>
+    ): Boolean {
+        for (transition in stateTransition.transitions) {
+            val rawDocument = HashMap(transition.toObject())
+            rawDocument.remove("\$action")
+            rawDocument["\$protocolVersion"] = stateTransition.protocolVersion
+            rawDocument["\$ownerId"] = stateTransition.ownerId
+            val rawDocumentFromProofBytes = values[ByteArrayKey(transition.id.toBuffer())]
+            if (rawDocumentFromProofBytes != null) {
+                val rawDocumentFromProof = Cbor.decode(rawDocumentFromProofBytes)
+                when (transition) {
+                    is DocumentCreateTransition -> {
+                        rawDocument.remove("\$entropy")
+                        rawDocument["\$revision"] = 1
+                    }
+                    is DocumentReplaceTransition -> {
+                        rawDocumentFromProof.remove("\$createdAt")
+                    }
+                    is DocumentDeleteTransition -> {
+
+                    }
+                }
+                return compareMaps(rawDocumentFromProof, rawDocument)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    protected fun verifyDataContactCreateTransition(
+        value: ByteArray,
+        stateTransition: DataContractCreateTransition
+    ): Boolean {
+        val match = Cbor.decode(value) == stateTransition.dataContract.toObject()
+        if (!match) {
+            return false
+        }
+        return true
+    }
+
+    protected fun verifyIdentityCreateTransition(
+        stateTransition: IdentityCreateTransition,
+        value: ByteArray
+    ): Boolean {
+        val rawIdentity = stateTransition.toObject()
+        rawIdentity["id"] = stateTransition.identityId.toBuffer()
+        rawIdentity["revision"] = 0
+        rawIdentity.remove("signature")
+        rawIdentity.remove("assetLockProof")
+        rawIdentity.remove("type")
+
+        val rawIdentityFromProof = Cbor.decode(value)
+        rawIdentityFromProof.remove("balance")
+        return rawIdentityFromProof == rawIdentity
     }
 }
