bb8bed412d389472023d1100a4c9a8c727be9a23
HashEngineering <hashengineeringsolutions@gmail.com>
2021-07-16 06:32:55 -0700

    add response classes

---
+++ src/main/kotlin/org/dashj/platform/dapiclient/model/GetIdentityIdsByPublicKeyHashesResponse.kt
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021-present, Dash Core Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package org.dashj.platform.dapiclient.model
+
+import org.dash.platform.dapi.v0.PlatformOuterClass
+
+class GetIdentityIdsByPublicKeyHashesResponse(val identityIds: List<ByteArray>, proof: Proof, metadata: ResponseMetadata) :
+    AbstractResponse(proof, metadata) {
+    constructor(response: PlatformOuterClass.GetIdentityIdsByPublicKeyHashesResponse) : this(response.identityIdsList.map { it.toByteArray() }, Proof(response.proof), ResponseMetadata(response.metadata))
+}
---
+++ src/main/kotlin/org/dashj/platform/dapiclient/model/GetIdentityResponse.kt
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021-present, Dash Core Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package org.dashj.platform.dapiclient.model
+
+import org.dash.platform.dapi.v0.PlatformOuterClass
+
+class GetIdentityResponse(val identity: ByteArray, proof: Proof, metadata: ResponseMetadata) :
+    AbstractResponse(proof, metadata) {
+    constructor(response: PlatformOuterClass.GetIdentityResponse) : this(response.identity.toByteArray(), Proof(response.proof), ResponseMetadata(response.metadata))
+}
