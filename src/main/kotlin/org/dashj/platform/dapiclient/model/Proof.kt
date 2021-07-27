c696a8e6655b1d7430202d90538dc174ed0a6b98
HashEngineering <hashengineeringsolutions@gmail.com>
2021-07-27 10:50:11 -0700

    fix: add isValid to Proof

--- src/main/kotlin/org/dashj/platform/dapiclient/model/Proof.kt
+++ src/main/kotlin/org/dashj/platform/dapiclient/model/Proof.kt
@@ -10,8 +10,11 @@ package org.dashj.platform.dapiclient.model
 import org.dashj.platform.dpp.toHexString
 
 data class Proof(val rootTreeProof: ByteArray, val storeTreeProof: ByteArray, val signatureLlmqHash: ByteArray, val signature: ByteArray) {
-    constructor(proof: org.dash.platform.dapi.v0.PlatformOuterClass.Proof) :
-    this(proof.rootTreeProof.toByteArray(), proof.storeTreeProof.toByteArray(), proof.signatureLlmqHash.toByteArray(), proof.signature.toByteArray())
+    constructor(proof: org.dash.platform.dapi.v0.PlatformOuterClass.Proof) : this(proof.rootTreeProof.toByteArray(), proof.storeTreeProof.toByteArray(), proof.signatureLlmqHash.toByteArray(), proof.signature.toByteArray())
+
+    fun isValid(): Boolean {
+        return rootTreeProof.isNotEmpty() && signature.isNotEmpty()
+    }
 
     override fun toString(): String {
         return "Proof(rootTreeProof: ${rootTreeProof.toHexString()}\n  storeTreeProof: ${storeTreeProof.toHexString()}\n  signatureLlmqHash: ${signatureLlmqHash.toHexString()}\n  signature: ${signature.toHexString()}"
