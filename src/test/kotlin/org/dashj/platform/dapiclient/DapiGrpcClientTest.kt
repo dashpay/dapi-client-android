e8ad3ebf7ddd28b58f11334db397ac30b074f949
HashEngineering <hashengineeringsolutions@gmail.com>
2021-09-08 09:11:03 -0700

    chore: remove comments referencing obsolete networks

--- src/test/kotlin/org/dashj/platform/dapiclient/DapiGrpcClientTest.kt
+++ src/test/kotlin/org/dashj/platform/dapiclient/DapiGrpcClientTest.kt
@@ -74,7 +74,6 @@ class DapiGrpcClientTest {
         val nodes = listOf("127.0.0.1", masternodeList[-0])
         val client = DapiClient(nodes)
         try {
-            // devnet-mobile, devnet genesis block
             val block1 = when (PARAMS) {
                 is DevNetParams -> (PARAMS as DevNetParams).devNetGenesisBlock
                 is TestNet3Params -> Block(PARAMS, Utils.HEX.decode("020000002cbcf83b62913d56f605c0e581a48872839428c92e5eb76cd7ad94bcaf0b00007f11dcce14075520e8f74cc4ddf092b4e26ebd23b8d8665a1ae5bfc41b58fdb4c3a95e53ffff0f1ef37a00000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0a510101062f503253482fffffffff0100743ba40b0000002321020131f38ae3eb0714531dbfc3f45491b4131d1211e3777177636388bb5a74c3e4ac00000000"))
@@ -147,7 +146,6 @@ class DapiGrpcClientTest {
     fun getDocumentsTest() {
         val client = DapiClient(masternodeList.toList())
         try {
-            // devnet-evonet
             val query = DocumentQuery.Builder()
                 .where(listOf("normalizedParentDomainName", "==", "dash").toMutableList())
                 // .where(listOf("normalizedLabel", "startsWith", "test").toMutableList())
