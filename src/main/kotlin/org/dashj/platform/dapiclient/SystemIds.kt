2e8674caa029172f5cbf6580a755abb9ea951086
HashEngineering <hashengineeringsolutions@gmail.com>
2022-01-25 14:29:45 -0800

    feat: add static contract and owner id identifiers

---
+++ src/main/kotlin/org/dashj/platform/dapiclient/SystemIds.kt
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2022-present, Dash Core Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+
+package org.dashj.platform.dapiclient
+
+import org.dashj.platform.dpp.identifier.Identifier
+
+object SystemIds {
+    val dpnsOwnerId = Identifier.from("4EfA9Jrvv3nnCFdSf7fad59851iiTRZ6Wcu6YVJ4iSeF")
+    val dpnsDataContractId = Identifier.from("GWRSAVFMjXx8HpQFaNJMqBV7MBgMK4br5UESsB4S31Ec")
+    val dashpayOwnerId = Identifier.from("5PhRFRrWZc5Mj8NqtpHNXCmmEQkcZE8akyDkKhsUVD4k")
+    val dashpayDataContractId = Identifier.from("Bwr4WHCPz5rFVAD87RqTs3izo4zpzwsEdKPWUT1NS1C7")
+}
